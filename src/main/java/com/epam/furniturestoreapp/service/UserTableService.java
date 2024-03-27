ebf7bdbefdc16691ac7f8aceba82390a043a1308
Yevgen <kulakov.ewgen@gmail.com>
2024-03-27 16:43:12 +0400

    Made UserTable controller and service

--- src/main/java/com/epam/furniturestoreapp/service/UserTableService.java
+++ src/main/java/com/epam/furniturestoreapp/service/UserTableService.java
@@ -1,7 +1,36 @@
 package com.epam.furniturestoreapp.service;
 
+import com.epam.furniturestoreapp.entity.UserTable;
+import com.epam.furniturestoreapp.repo.UserTableRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserTableService {
+    private final UserTableRepository userTableRepository;
+
+    @Autowired
+    public UserTableService(UserTableRepository userTableRepository){
+        this.userTableRepository = userTableRepository;
+    }
+
+    public void addUser(UserTable user) {
+        userTableRepository.save(user);
+    }
+
+    public boolean existsById(long id) {
+        return userTableRepository.existsById(id);
+    }
+
+    public UserTable findById(long id) {
+        return userTableRepository.findById(id).orElse(null);
+    }
+
+    public void updateUser(UserTable user) {
+        userTableRepository.save(user);
+    }
+
+    public void deleteById(long id) {
+        userTableRepository.deleteById(id);
+    }
 }
