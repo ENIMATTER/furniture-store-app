451b8439458ff83845a86e134e45c153da681042
Yevgen <kulakov.ewgen@gmail.com>
2024-04-02 19:24:54 +0400

    Make searching, ordering and filtering by color, price and material

--- src/main/java/com/epam/furniturestoreapp/controller/ShopController.java
+++ src/main/java/com/epam/furniturestoreapp/controller/ShopController.java
@@ -4,27 +4,26 @@ import com.epam.furniturestoreapp.entity.Category;
 import com.epam.furniturestoreapp.entity.Product;
 import com.epam.furniturestoreapp.service.CategoryService;
 import com.epam.furniturestoreapp.service.ProductService;
-import com.epam.furniturestoreapp.util.FilterUtil;
+import com.epam.furniturestoreapp.util.Color;
+import com.epam.furniturestoreapp.util.Material;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@RequestMapping("/products")
 @Controller
 public class ShopController {
     private final CategoryService categoryService;
     private final ProductService productService;
-    private static List<String> filterList;
-    private String lastSearch = "";
+    private static final List<String> filterList;
+    private static final Map<Color, String> colorMap;
+    private static final List<Material> materialList;
 
     static {
         filterList = new ArrayList<>();
-        filterList.add("Select filter:");
         filterList.add("Last added");
         filterList.add("By rating");
         filterList.add("A-Z");
@@ -33,52 +32,80 @@ public class ShopController {
         filterList.add("From highest price");
     }
 
+    static {
+        colorMap = new HashMap<>();
+        colorMap.put(Color.White, "background-color:#e0dfdf;");
+        colorMap.put(Color.Blue, "background-color:#0b5fb5;");
+        colorMap.put(Color.Green, "background-color:#00a651;");
+        colorMap.put(Color.Yellow, "background-color:#f7ff01;");
+        colorMap.put(Color.Red, "background-color:#ff0000;");
+        colorMap.put(Color.Black, "background-color:#000000;");
+        colorMap.put(Color.Brown, "background-color:#65270c;");
+    }
+
+    static {
+        materialList = new ArrayList<>();
+        materialList.add(Material.Wood);
+        materialList.add(Material.Metal);
+        materialList.add(Material.Glass);
+        materialList.add(Material.Leather);
+        materialList.add(Material.Textile);
+    }
+
     @Autowired
-    public ShopController(CategoryService categoryService, ProductService productService){
+    public ShopController(CategoryService categoryService, ProductService productService) {
         this.categoryService = categoryService;
         this.productService = productService;
     }
 
-    @GetMapping("/products")
-    public String getAllProducts(Model model){
-        List<Category> categories = categoryService.findAll();
-        List<Product> products = productService.getAllProducts();
-        model.addAttribute("categories", categories);
-        model.addAttribute("products", products);
-        model.addAttribute("filterList", filterList);
-        model.addAttribute("lastSearch", lastSearch);
-        return "shop";
-    }
-
-    @GetMapping("/products/{id}")
-    public String getProductsByCategoryId(@PathVariable long id, Model model){
+    @GetMapping("/category/{id}")
+    public String getProductsByCategoryId(@PathVariable long id, Model model) {
         List<Category> categories = categoryService.findAll();
         model.addAttribute("categories", categories);
         Category category = categoryService.findById(id);
-        if(category == null){
+        if (category == null) {
             return "not-found";
         }
         List<Product> products = productService.getAllProductsByCategory(category);
         model.addAttribute("products", products);
         model.addAttribute("filterList", filterList);
-        model.addAttribute("lastSearch", lastSearch);
+        model.addAttribute("colorMap", colorMap);
+        model.addAttribute("materialList", materialList);
         return "shop";
     }
 
-    @PostMapping("/products")
-    public String getProductsByName(@ModelAttribute("filterAndSearch") FilterUtil filterAndSearch,
-                                    Model model){
-
+    @GetMapping
+    public String getProducts(Model model) {
         List<Category> categories = categoryService.findAll();
-        model.addAttribute("categories", categories);
-
         List<Product> products = productService.getAllProducts();
+        model.addAttribute("categories", categories);
+        model.addAttribute("products", products);
+        model.addAttribute("filterList", filterList);
+        model.addAttribute("colorMap", colorMap);
+        model.addAttribute("materialList", materialList);
+        return "shop";
+    }
 
-        if(filterAndSearch.getFilter() == null){
-            filterAndSearch.setFilter("Select filter:");
+    @PostMapping
+    public String postProducts(@RequestParam(value = "search", required = false) String search,
+                                  @RequestParam(value = "filter", defaultValue = "Last added") String filter,
+                                  @RequestParam(value = "from", required = false) Double from,
+                                  @RequestParam(value = "to", required = false) Double to,
+                                  @RequestParam(value = "color", required = false) Color color,
+                                  @RequestParam(value = "materials", required = false) Material[] materials,
+                                  Model model) {
+        List<Category> categories = categoryService.findAll();
+        List<Product> products;
+
+        // Search
+        if (search == null || search.isEmpty()) {
+            products = productService.getAllProducts();
+        } else {
+            products = productService.getAllProductsByName(search);
         }
 
-        switch (filterAndSearch.getFilter()) {
+        // Filter order
+        switch (filter) {
             case "By rating" -> products.sort(((o1, o2) -> (int) (o2.getAverageRating() - o1.getAverageRating())));
             case "Last added" -> Collections.reverse(products);
             case "A-Z" -> products.sort((Comparator.comparing(Product::getProductName)));
@@ -86,35 +113,30 @@ public class ShopController {
             case "From highest price" -> products.sort(((o1, o2) -> (int) (o2.getPrice() - o1.getPrice())));
             case "From lowest price" -> products.sort((Comparator.comparingDouble(Product::getPrice)));
         }
+        filterList.remove(filter);
+        filterList.add(0, filter);
 
-        filterList.set(0, filterAndSearch.getFilter());
+        // Filter price
+        if(from != null && to != null){
+            products = productService.getAllProductsInRange(from, to);
+        }
 
-        model.addAttribute("products", products);
-        model.addAttribute("filterList", filterList);
-        model.addAttribute("lastSearch", lastSearch);
+        // Filter color
+        if(color != null){
+            products = productService.getAllProductsByColor(color);
+        }
 
-        return "shop";
-    }
+        // Filter materials
+        if(materials != null){
+            products = productService.getAllProductsByMaterial(materials);
+        }
 
-    @PostMapping("/products/search")
-    public String searchProductsByName(@ModelAttribute("name") String name){
-        return "redirect:/products/search/" + name;
-    }
-
-    @GetMapping("/products/search/{name}")
-    public String getSearchProductsByName(Model model, @PathVariable String name){
-
-        List<Category> categories = categoryService.findAll();
         model.addAttribute("categories", categories);
-
-        List<Product> products = productService.getAllProductsByName(name);
-        lastSearch = name;
-
-        model.addAttribute("products", products);
         model.addAttribute("filterList", filterList);
-        model.addAttribute("lastSearch", lastSearch);
+        model.addAttribute("products", products);
+        model.addAttribute("colorMap", colorMap);
+        model.addAttribute("materialList", materialList);
 
         return "shop";
     }
-
 }
--- src/main/java/com/epam/furniturestoreapp/service/ProductService.java
+++ src/main/java/com/epam/furniturestoreapp/service/ProductService.java
@@ -3,9 +3,12 @@ package com.epam.furniturestoreapp.service;
 import com.epam.furniturestoreapp.entity.Category;
 import com.epam.furniturestoreapp.entity.Product;
 import com.epam.furniturestoreapp.repo.ProductRepository;
+import com.epam.furniturestoreapp.util.Color;
+import com.epam.furniturestoreapp.util.Material;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -27,5 +30,33 @@ public class ProductService {
 
     public List<Product> getAllProductsByName(String name) {
         return productRepository.getAllByProductName(name);
+    }
+
+    public List<Product> getAllProductsInRange(Double from, Double to) {
+        List<Product> productList = productRepository.findAll();
+        productList.removeIf(p -> p.getPrice() < from || p.getPrice() > to);
+        return productList;
+    }
+
+    public List<Product> getAllProductsByColor(Color color) {
+        List<Product> productList = productRepository.findAll();
+        productList.removeIf(p -> !color.name().equals(p.getColor()));
+        return productList;
+    }
+
+    public List<Product> getAllProductsByMaterial(Material[] material) {
+        List<Product> productList = productRepository.findAll();
+        List<Product> result = new ArrayList<>();
+        List<String> materialStrings = new ArrayList<>();
+        for(Material m : material){
+            materialStrings.add(m.name());
+        }
+        for(Product p : productList){
+            String[] productMaterials = p.getMaterial().split(",");
+            if(List.of(productMaterials).containsAll(materialStrings)){
+                result.add(p);
+            }
+        }
+        return result;
     }
 }
